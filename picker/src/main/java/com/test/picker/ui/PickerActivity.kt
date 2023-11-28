4603fd6167e9b8ea4bb373aea366cff07149be00
Vladyslav Yanovych <yanovich30@gmail.com>
2023-11-29 00:02:35 +0200

    fix bugs with insets

--- picker/src/main/java/com/test/picker/ui/PickerActivity.kt
+++ picker/src/main/java/com/test/picker/ui/PickerActivity.kt
@@ -1,30 +1,32 @@
 package com.test.picker.ui
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.test.common.base.BaseActivity
 import com.test.picker.R
 import com.test.picker.adapter.PickerAdapter
 import com.test.picker.databinding.ActivityPickerBinding
 
-class PickerActivity : AppCompatActivity(R.layout.activity_picker) {
+class PickerActivity : BaseActivity<ActivityPickerBinding>(ActivityPickerBinding::inflate) {
 
     private val list by lazy { intent.getStringArrayListExtra(EXTRA_LIST) }
 
     private val title by lazy { intent.getStringExtra(EXTRA_TITLE) }
 
-    private val binding: ActivityPickerBinding by viewBinding()
-
     private var mAdapter: PickerAdapter? = null
 
     private var pick: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun initView() {
         setContentView(binding.root)
         initList()
         initBack()
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        statusBarInset(binding.root)
+        navInset(binding.materialButton)
     }
 
     private fun initBack() {
